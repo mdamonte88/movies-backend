db47273c3b98ab55f23e072eef124804f006257f
Matias Damonte <mdamonte88@hotmail.com>
2020-09-15 12:50:20 -0300

    Changes the port of server to 5000

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000");
-});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
