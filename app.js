0e30bffebb072cbee4cea8ae426e69f96336951f
Matias Damonte <mdamonte88@hotmail.com>
2020-09-15 13:22:39 -0300

    Connects the server with MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const app = express();
+connectDB();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
