57cc7c172a093a89b607d78d491be3855c737e02
Matias Damonte <mdamonte88@hotmail.com>
2020-09-23 16:20:50 -0300

    Allows to integrate a compiled source code of our React App It allows out client site app navigates between pages and process url correctly

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const connectDB = require('./config/db');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const logger = require("morgan")
-connectDB();
+const logger = require("morgan");
+const path = require('path');
 
+connectDB();
+//Server build folder of a Frontend app in React
+app.use(express.static(path.join(__dirname, '/build')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -17,5 +20,9 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/movies', require('./routes/api/movies_routes'));
 app.use('/api/schedules', require('./routes/api/schedules_routes'));
 
+//Redirect any requests that are caught by any of other Api routes should be passes on to our app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'))
+})
 
 module.exports = app;
